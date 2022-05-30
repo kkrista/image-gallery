76718ee0c0a441df108c7363c285ed8e213cbbfc
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-05-30 17:41:16 +0300

    Contact information edited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import PictureBrowse from "./components/PictureBrowse";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <PictureBrowse defaultKeyWord="Pictures" />
+      </main>
+      <footer>
+        <a href="https://github.com/kkrista/image-gallery">
+          Open-source code
+        </a>{" "}
+        by Krista-Mari Katajisto
+      </footer>
     </div>
   );
 }
